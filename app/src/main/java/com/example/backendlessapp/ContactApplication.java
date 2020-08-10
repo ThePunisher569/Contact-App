c10b58d1b644b6080877859e58c0449e0b5d0f6d
Momin Aatif <aatifdom10@gmail.com>
2020-08-10 16:00:09 +0530

    Update ContactApplication.java

--- app/src/main/java/com/example/backendlessapp/ContactApplication.java
+++ app/src/main/java/com/example/backendlessapp/ContactApplication.java
@@ -8,8 +8,8 @@ import com.backendless.BackendlessUser;
 import java.util.List;
 
 public class ContactApplication extends Application {
-    private static final String APP_ID = "E39B54DA-A82F-DE17-FF21-A5DA081FF000";
-    private static final String API_KEY="17BE1BD7-D02E-BFEC-FF51-E8181B765400";
+    private static final String APP_ID = "YOUR_APP_ID";
+    private static final String API_KEY="YOUR_API_KEY";
     private static final String SERVER_URL="https://api.backendless.com";
 
     public static BackendlessUser user;
